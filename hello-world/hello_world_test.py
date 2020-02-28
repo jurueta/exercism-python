2795f8426617c75c97c09adbd5d18fe05fa315ae
Jesus Urueta <juruetacantillo@gmail.com>
2020-02-27 20:28:02 -0500

    Commit inicial con 2 ejercicios

---
+++ hello-world/hello_world_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from hello_world import hello
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
+
+
+class HelloWorldTest(unittest.TestCase):
+    def test_say_hi(self):
+        self.assertEqual(hello(), "}
+    ")
+
+
+if __name__ == "__main__":
+    unittest.main()
